3970c8fee7c4fb00622833e760ebe2db9b40c612
Florian Beyer <mail@flobeyer.tk>
2017-06-23 13:00:22 +0200

    Update ExtractLandsat.py

--- ExtractLandsat.py
+++ ExtractLandsat.py
@@ -1,7 +1,24 @@
+''' 
+
 #### Florian Beyers script to extract Landsat-Data vom *.tar.gz in a folder
 ### with Python 2.7
 ## 2017-05-24
-# mail@flobeyer.tk
+# mail@flobeyer.de
+
+   Copyright 2015 Florian Beyer
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+       http://www.apache.org/licenses/LICENSE-2.0
+       
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ '''
+
 
 # import packages
 import tarfile
